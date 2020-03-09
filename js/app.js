fc31b1c512ed9b97a574661d138dbb7ae9de8195
Stacey Teltser <stacey.teltser@gmail.com>
2020-03-09 12:01:23 -0500

    added more js functionality for pizza vote counter

--- js/app.js
+++ js/app.js
@@ -1,14 +1,28 @@
 'use strict';
 console.log('This is the pizza tracker');
+var img1Clicked = 0;
+var img2Clicked = 0;
 
 function imageWasClicked(){
-
   console.log('image was clicked');
+
+  if(event.srcElement.id === '1'){
+    img1Clicked++;
+  } else if (event.srcElement.id === '2'){
+    img2Clicked++;
+  }
+
+  if(img1Clicked + img2Clicked >= 5){
+    var footerElement = document.getElementsByTagName('footer')[0];
+    footerElement.textContent = `You picked pizza 1 ${img1Clicked} times and pizza 2 ${img2Clicked} times.`;
+}
+
 }
 
 
 var imageElements = document.getElementsByTagName('img');
 
 for(var i = 0; i < imageElements.length; i++){
+  console.log('this is the event listener for the click on pizza event');
   imageElements[i].addEventListener('click', imageWasClicked);
 }
