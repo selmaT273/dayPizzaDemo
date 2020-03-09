8eea0a9fb4d273d6a403662328f1c6ee4fa7993f
Stacey Teltser <stacey.teltser@gmail.com>
2020-03-09 11:42:58 -0500

    added content to main and event listener

--- js/app.js
+++ js/app.js
@@ -1,2 +1,14 @@
 'use strict';
 console.log('This is the pizza tracker');
+
+function imageWasClicked(){
+
+  console.log('image was clicked');
+}
+
+
+var imageElements = document.getElementsByTagName('img');
+
+for(var i = 0; i < imageElements.length; i++){
+  imageElements[i].addEventListener('click', imageWasClicked);
+}
